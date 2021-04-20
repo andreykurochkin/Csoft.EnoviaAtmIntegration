35cdd38e146411a7b1e84d019e9d9a0e55f0de3e
andreykurochkin <andrey.kurochkin@hotmail.com>
2021-04-20 19:50:34 +0300

    modified constructor of TdmsAttributeValueBehavior

--- ApplicationReport/TdmsAttributeValueBehavior.cs
+++ ApplicationReport/TdmsAttributeValueBehavior.cs
@@ -2,18 +2,8 @@
 
 namespace Csoft.EnoviaAtmIntegration.Domain {
     public abstract class TdmsAttributeValueBehavior<TResult> {
-        protected TDMSAttribute attribute;
-        protected InvocationSafe<TDMSAttribute, TResult> value;
-        public TdmsAttributeValueBehavior(TDMSAttribute attribute) {
-            this.attribute = attribute;
-        }
-        public TResult GetValue() {
-            return value.Invoke();
-        }
-    }
-    public abstract class AlterTdmsAttributeValueBehavior<TResult> {
         protected IInvokable<TDMSAttribute, TResult> Invocation { get; }
-        public AlterTdmsAttributeValueBehavior(TDMSAttribute attribute, IInvokable<TDMSAttribute, TResult> invocation) {
+        public TdmsAttributeValueBehavior(TDMSAttribute attribute, IInvokable<TDMSAttribute, TResult> invocation) {
             Invocation = invocation;
         }
         public TResult GetValue() {
